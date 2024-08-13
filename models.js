33f07e4ff2d4890512f7b0754ea3eec23ffb103c
Jayben2145 <109489641+Jayben2145@users.noreply.github.com>
2024-08-14 11:28:41 +1200

    Update models.js

--- models.js
+++ models.js
@@ -3,7 +3,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 // Replace with your actual database connection details
 const sequelize = new Sequelize('blogdb', 'remote', 'burlywood995', {
   host: '172.0.0.1', // Replace with the IP address of your SQL server
-  port: 3306, // Replace with the port number of your SQL server (default for MySQL is 3306)
+  port: 15306, // Replace with the port number of your SQL server (default for MySQL is 3306)
   dialect: 'mysql', // Change this to the appropriate dialect (e.g., 'mysql', 'postgres', 'mssql')
   logging: false, // Set to true if you want to see SQL logs
 });
